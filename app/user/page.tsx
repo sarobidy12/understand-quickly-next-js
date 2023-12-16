28129aac68b8b61a7b0404b0a772d9449f14393b
sarobidy12 <sarobidyalliance@outlook.com>
2023-12-16 12:33:03 +0300

    dynamique route

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,21 +1,32 @@
 import React, { Suspense } from "react";
 import Card from "../Component/Card/User";
+import Head from "next/head";
 
 const User = async () => {
   const data: any[] = await getStaticProps();
   return (
-    <div>
-      <div className="my-4">
-        <h1 className="text-2xl font-bold">List user.</h1>
-        <p>This is un simple project with next.js, how use dynamique route</p>
-      </div>
+    <>
+      <Head>
+        <title>List user.</title>
+      </Head>
+      <div>
+        <div className="my-4">
+          <h1 className="text-2xl font-bold">List user.</h1>
+          <p>This is un simple project with next.js, how use dynamique route</p>
+        </div>
 
-      <div className="grid grid-cols-4 gap-4">
-        {data.map((x: any) => (
-          <Card to={`/user/${x.id}`} name={x.name} mail={x.email} key={x.id} />
-        ))}
+        <div className="grid grid-cols-4 gap-4">
+          {data.map((x: any) => (
+            <Card
+              to={`/user/${x.id}`}
+              name={x.name}
+              mail={x.email}
+              key={x.id}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
